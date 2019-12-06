63e2eff3527a647ca1c3db1eb37a3c7d24f4dc3f
Jasmin Chermak <jasminchermak@Jasmins-MacBook-Air.local>
2019-12-05 22:47:58 -0800

    Adding June and December data.

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def temp_monthly():
 def stats(start=None, end=None):
     sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
     if not end:
-        results = session.query(*sel).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+        results = session.query(*sel).filter(Measurement.date >= start).all()
         temps = list(np.ravel(results))
         return jsonify(temps)
     results = session.query(*sel).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
